b94073835d6edcb9fd5fb30c9328261652df1efd
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-10-17 21:26:53 -0500

    initialize firebase

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCaQjjYotpY-p9HHMxZx4hdaiRel8HwUvo",
+    authDomain: "movienight-crusaders.firebaseapp.com",
+    databaseURL: "https://movienight-crusaders.firebaseio.com",
+    projectId: "movienight-crusaders",
+    storageBucket: "movienight-crusaders.appspot.com",
+    messagingSenderId: "471511407868"
+  };
+  firebase.initializeApp(config);
