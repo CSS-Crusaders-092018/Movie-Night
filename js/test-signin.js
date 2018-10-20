002aba4488819480707132282347f6388cc813a9
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-10-20 11:28:00 -0500

    retesting auth with firebase project account

--- js/test-signin.js
+++ js/test-signin.js
@@ -1,10 +1,33 @@
 // Initialize Firebase - movie crusaders account
-var config = {
-    apiKey: "AIzaSyB3ognBnBLe-vgaHhsZV7ksufHgzg21VFs",
-    authDomain: "movie-night-464be.firebaseapp.com",
-    databaseURL: "https://movie-night-464be.firebaseio.com",
-    projectId: "movie-night-464be",
-    storageBucket: "movie-night-464be.appspot.com",
-    messagingSenderId: "123201978661"
-  };
-  firebase.initializeApp(config);
+$(document).ready(function(){
+
+    var config = {
+        apiKey: "AIzaSyB3ognBnBLe-vgaHhsZV7ksufHgzg21VFs",
+        authDomain: "movie-night-464be.firebaseapp.com",
+        databaseURL: "https://movie-night-464be.firebaseio.com",
+        projectId: "movie-night-464be",
+        storageBucket: "movie-night-464be.appspot.com",
+        messagingSenderId: "123201978661"
+    };
+    firebase.initializeApp(config);
+
+    $('#submit').on('click', function(){
+        var email = $('#email').val();
+        var password = $('#password').val();
+       
+        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log(errorCode)
+            console.log(errorMessage);
+            // ...
+          });
+    });
+
+
+
+
+
+
+});
