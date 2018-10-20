9fffbd84900ba542dd338a73feabd039f231fe29
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-10-20 10:56:22 -0500

    added sign-in to template for testing

--- js/test-login-index.js
+++ js/test-login-index.js
@@ -17,8 +17,8 @@ $(document).ready(function(){
 
 
     $('#submit').on('click', function(){
-        var email = $('#email').val();
-        var password = $('#password').val();
+        var email = $('#newEmail').val();
+        var password = $('#newPassword').val();
        
         firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
             // Handle Errors here.
